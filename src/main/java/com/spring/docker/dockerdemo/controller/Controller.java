1bfe04d9e255cb2332e431b6474555560342177b
GADO89 <gadomahmoud377@gmail.com>
2023-12-11 05:35:37 +0200

    envionment variable

--- src/main/java/com/spring/docker/dockerdemo/controller/Controller.java
+++ src/main/java/com/spring/docker/dockerdemo/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.spring.docker.dockerdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Controller {
 
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private String age;
     @GetMapping("/getMessage")
     public String getMessage(){
-        return "getMessage";
+        return "Hi Student "+name+" with age "+age;
     }
 }
